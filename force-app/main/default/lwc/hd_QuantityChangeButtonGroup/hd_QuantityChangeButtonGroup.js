021f48dcd4e7d6156ec02ac9ba3c232278763e4b
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-16 16:25:26 +0800

    update: use utility class instead of classname

--- force-app/main/default/lwc/hd_QuantityChangeButtonGroup/hd_QuantityChangeButtonGroup.js
+++ force-app/main/default/lwc/hd_QuantityChangeButtonGroup/hd_QuantityChangeButtonGroup.js
@@ -3,23 +3,24 @@ import { LightningElement } from 'lwc';
 export default class hd_QuantityfChangeButtonGroup extends LightningElement {
     number = 0;
 
-    renderedCallback() {
-        // this.number = 0;
-    }
-
     increment() {
-        console.log('>> In increment function');
         this.number += 1;
     }
 
     decrement() {
-        console.log('>> In decrement function');
         if (this.number !== 0) {
             this.number -= 1;
         }
     }
-
+    decimalCheck(event) {
+        if (event.key === '.' || event.key === 'e' || event.key === 'E') {
+            event.preventDefault();
+        }
+    }
     onQuantityChange(event) {
-        console.log(event);
+        if (!isNaN(event.target.value)) {
+            // this.number = parseInt(event.target.value);
+            this.number = Number(event.target.value);
+        }
     }
 }

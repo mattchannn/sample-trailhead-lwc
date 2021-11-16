9c3e03216a2ba4b5882e112d11bf7e28c3e0cf0b
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-16 18:12:20 +0800

    init: test static resources

--- force-app/main/default/lwc/hd_QuantityChangeButtonGroup/hd_QuantityChangeButtonGroup.js
+++ force-app/main/default/lwc/hd_QuantityChangeButtonGroup/hd_QuantityChangeButtonGroup.js
@@ -1,7 +1,32 @@
 import { LightningElement } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import externalStyle from '@salesforce/resourceUrl/hd_QuantityChangeButtonGroupAlignInput';
 
 export default class hd_QuantityfChangeButtonGroup extends LightningElement {
     number = 0;
+
+    renderedCallback() {
+        console.log('>> hd_QuantityChangeButtonGroup rendered..');
+        loadStyle(
+            this,
+            externalStyle + '/hd_QuantityChangeButtonGroupAlignInput.css'
+        )
+            .then(() => {
+                console.log(
+                    'hd_QuantityChangeButtonGroupAlignInput external css file loaded'
+                );
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error loading hd_QuantityChangeButtonGroupAlignInput',
+                        message: error.message,
+                        variant: 'error'
+                    })
+                );
+            });
+    }
 
     increment() {
         this.number += 1;

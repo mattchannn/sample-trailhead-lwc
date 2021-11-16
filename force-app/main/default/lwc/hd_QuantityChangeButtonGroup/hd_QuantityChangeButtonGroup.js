ac25686da1eea1392499217cc45e34451f6af00e
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-16 11:06:39 +0800

    update: QuantityChangeButtonGroup

--- force-app/main/default/lwc/hd_QuantityChangeButtonGroup/hd_QuantityChangeButtonGroup.js
+++ force-app/main/default/lwc/hd_QuantityChangeButtonGroup/hd_QuantityChangeButtonGroup.js
@@ -1,28 +1,23 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class hd_TrackDecoratorDemo extends LightningElement {
-
-    @track fullName = { firstName : '', lastName : ''};
+    number = 0;
 
     renderedCallback() {
-        console.log(">> RenderedCallback: render now");
+        // this.number = 0;
     }
 
-    handleClick(event) {
-        console.log(">> handleClick...");
-        
-        // This will not re-render unless you have @track decorator
-        // this.fullName.firstName = 'John';
-        // this.fullName.lastName = 'Joe';
+    increment() {
+        this.number += 1;
+    }
 
+    decrement() {
+        if (this.number !== 0) {
+            this.number -= 1;
+        }
+    }
 
-        // This will not re-render if the object value is not used in the component
-        // E.g.: Removing <p class="slds-p-horizontal_small">Middle name: {fullName.middleName}</p>
-        this.fullName.middleName = 'Whatever';
-
-        // This will re-render even without the @track decorator since SF detects mutation
-        // this.fullName = {...this.fullName, firstName: "John", lastName: "Joe"}
-        
-        console.log(">> handleClick: fullName is ", JSON.stringify(this.fullName, null, 2));
+    onQuantityChange(event) {
+        console.log(event.target);
     }
 }

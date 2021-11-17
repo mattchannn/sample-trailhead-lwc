159bb7e1e4c85a9000b6d37d46a1bdf802e4e4f5
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-17 14:47:08 +0800

    init: add new changeButtonGroupClone
    - to see if there is an effect to newly created component with the same class name

---
+++ force-app/main/default/lwc/hd_QuantityChangeButtonGroupClone/hd_QuantityChangeButtonGroupClone.js
@@ -0,0 +1,50 @@
+import { LightningElement } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import externalComponentStyle from '@salesforce/resourceUrl/hd_QuantityChangeButtonGroupAlignInput';
+
+export default class hd_QuantityfChangeButtonGroupClone extends LightningElement {
+    number = 0;
+
+    async connectedCallback() {
+        console.log('>> hd_QuantityChangeButtonGroup, connectedCallback, the component is populated to the DOM..');
+        // await this.loadGlobalStyle();
+    }
+
+    async loadGlobalStyle() {
+        try {
+            await loadStyle(this, externalComponentStyle);
+            console.log('hd_QuantityChangeButtonGroupAlignInput external css file loaded');
+        } catch (err) {
+            console.error(err);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error loading hd_QuantityChangeButtonGroupAlignInput',
+                    message: err.message,
+                    variant: 'error'
+                })
+            );
+        }
+    }
+
+    increment() {
+        this.number += 1;
+    }
+
+    decrement() {
+        if (this.number !== 0) {
+            this.number -= 1;
+        }
+    }
+    decimalCheck(event) {
+        if (event.key === '.' || event.key === 'e' || event.key === 'E') {
+            event.preventDefault();
+        }
+    }
+    onQuantityChange(event) {
+        if (!isNaN(event.target.value)) {
+            // this.number = parseInt(event.target.value);
+            this.number = Number(event.target.value);
+        }
+    }
+}

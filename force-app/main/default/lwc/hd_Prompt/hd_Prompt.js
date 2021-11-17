d87e5380a8d555f5926f7ff919e2ae8de3f13369
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-17 18:33:15 +0800

    fix: use dynamic param for prompt theme

--- force-app/main/default/lwc/hd_Prompt/hd_Prompt.js
+++ force-app/main/default/lwc/hd_Prompt/hd_Prompt.js
@@ -1,5 +1,26 @@
 import { LightningElement, api } from 'lwc';
 
 export default class hd_Prompt extends LightningElement {
+    @api mode;
     @api title;
+
+    headerBaseTheme = 'slds-modal__header slds-theme_alert-texture';
+    headerTheme;
+
+    connectedCallback() {
+        this.setHeaderTheme();
+    }
+
+    setHeaderTheme() {
+        switch (this.mode) {
+            case 'error':
+                this.headerTheme = this.headerBaseTheme + ' slds-theme_error';
+                break;
+            case 'warning':
+                this.headerTheme = this.headerBaseTheme + ' slds-theme_warning';
+                break;
+            default:
+                console.log('Default');
+        }
+    }
 }

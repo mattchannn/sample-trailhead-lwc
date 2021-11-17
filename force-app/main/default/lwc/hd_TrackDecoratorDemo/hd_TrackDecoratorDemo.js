80aabd9beb8555b0e9fa4a5e4d05e9a1035d36ae
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-17 15:50:22 +0800

    update<TrackDecoratorDemo>: styling issue plus on button click

--- force-app/main/default/lwc/hd_TrackDecoratorDemo/hd_TrackDecoratorDemo.js
+++ force-app/main/default/lwc/hd_TrackDecoratorDemo/hd_TrackDecoratorDemo.js
@@ -1,20 +1,30 @@
 import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class hd_TrackDecoratorDemo extends LightningElement {
-
-    @track fullName = { firstName : '', lastName : ''};
+export default class hd_TrackDecoratorDemo extends NavigationMixin(LightningElement) {
+    @track fullName = { firstName: '', lastName: '' };
 
     renderedCallback() {
-        console.log(">> RenderedCallback: render now");
+        console.log('>> hd_TrackDecoratorDemo: <renderedCallback()>: render now');
     }
 
-    handleClick(event) {
-        console.log(">> handleClick...");
-        
+    navigateToOrderDetailPage() {
+        console.log('>> hd_TrackDecoratorDemo: navigating to order detail page');
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Order',
+                actionName: 'list'
+            }
+        });
+    }
+
+    handleClick() {
+        console.log('>> handleClick...');
+
         // This will not re-render unless you have @track decorator
         // this.fullName.firstName = 'John';
         // this.fullName.lastName = 'Joe';
-
 
         // This will not re-render if the object value is not used in the component
         // E.g.: Removing <p class="slds-p-horizontal_small">Middle name: {fullName.middleName}</p>
@@ -22,7 +32,7 @@ export default class hd_TrackDecoratorDemo extends LightningElement {
 
         // This will re-render even without the @track decorator since SF detects mutation
         // this.fullName = {...this.fullName, firstName: "John", lastName: "Joe"}
-        
-        console.log(">> handleClick: fullName is ", JSON.stringify(this.fullName, null, 2));
+
+        console.log('>> handleClick: fullName is ', JSON.stringify(this.fullName, null, 2));
     }
 }

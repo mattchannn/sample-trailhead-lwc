fe5f51bd6134c47b6219d5fb0676f0fd67fdee36
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-22 15:57:10 +0800

    update: finish SpinnerDemo component

--- force-app/main/default/lwc/hd_SpinnerDemo/hd_SpinnerDemo.js
+++ force-app/main/default/lwc/hd_SpinnerDemo/hd_SpinnerDemo.js
@@ -1,4 +1,11 @@
 import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
-export default class hd_SpinnerDemo extends NavigationMixin(LightningElement) {}
+export default class hd_SpinnerDemo extends NavigationMixin(LightningElement) {
+    isLoading;
+
+    handleClick() {
+        // Toggle Loading
+        this.isLoading = !this.isLoading;
+    }
+}

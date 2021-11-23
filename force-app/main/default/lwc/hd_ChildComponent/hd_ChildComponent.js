0f4e40b636e257cbdbe9510f56b94f79511a491f
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-23 18:25:12 +0800

    styling: intermediate styling

--- force-app/main/default/lwc/hd_ChildComponent/hd_ChildComponent.js
+++ force-app/main/default/lwc/hd_ChildComponent/hd_ChildComponent.js
@@ -1,3 +1,7 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement } from 'lwc';
 
-export default class hd_ChildComponent extends LightningElement {}
+export default class hd_ChildComponent extends LightningElement {
+    handleClick() {
+        this.dispatchEvent(new CustomEvent('clicker', { bubbles: true, composed: true, detail: { message: 'The message is now here' } }));
+    }
+}

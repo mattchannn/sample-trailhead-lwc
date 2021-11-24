6162efeeaa88e9aaa66be7ba2520bcafe1d66981
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-24 14:13:38 +0800

    update: distinguish the bubble true and bubble false

--- force-app/main/default/lwc/hd_ChildComponent/hd_ChildComponent.js
+++ force-app/main/default/lwc/hd_ChildComponent/hd_ChildComponent.js
@@ -1,7 +1,10 @@
 import { LightningElement } from 'lwc';
 
 export default class hd_ChildComponent extends LightningElement {
-    handleClick() {
-        this.dispatchEvent(new CustomEvent('clicker', { bubbles: true, composed: true, detail: { message: 'The message is now here' } }));
-    }
+  message = '';
+
+  handleClick() {
+    this.message = 'Hurray';
+    this.dispatchEvent(new CustomEvent('clicker', { bubbles: false, composed: true, detail: { message: 'Hurray' } }));
+  }
 }
--- force-app/main/default/lwc/hd_ParentComponent/hd_ParentComponent.js
+++ force-app/main/default/lwc/hd_ParentComponent/hd_ParentComponent.js
@@ -1,3 +1,15 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement } from 'lwc';
 
-export default class hd_ParentComponent extends LightningElement {}
+export default class hd_ParentComponent extends LightningElement {
+  message;
+
+  childOnClickHandler(evt) {
+    console.log('>> childOnClickHandler: The event is now bubbled to hd_ParentComponent');
+    this.message = evt.detail.message + ' from childOnClickHandler';
+  }
+
+  divOnClickHandler(evt) {
+    console.log('>> divOnClickHandler: The event is now bubbled to hd_ParentComponent and then to the div as well');
+    this.message = evt.detail.message + ' from divOnClickHandler';
+  }
+}

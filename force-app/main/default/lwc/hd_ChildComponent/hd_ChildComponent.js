a3beba9cbd58a25e965e548e740bd28dee0e5b73
MatChan <48675050+mattchannn@users.noreply.github.com>
2021-11-24 16:24:58 +0800

    update: event bubble
    - finish the case for both internal event and event passed to slot

--- force-app/main/default/lwc/hd_ChildComponent/hd_ChildComponent.js
+++ force-app/main/default/lwc/hd_ChildComponent/hd_ChildComponent.js
@@ -4,7 +4,47 @@ export default class hd_ChildComponent extends LightningElement {
   message = '';
 
   handleClick() {
-    this.message = 'Hurray';
-    this.dispatchEvent(new CustomEvent('clicker', { bubbles: false, composed: true, detail: { message: 'Hurray' } }));
+    /**
+     * using dispatchEvent for external events
+     *
+     * the below scenario assumes the elements have corresponding event listener attached
+     * with bubbles: false > only shadow host will listen to the event
+     *
+     * with bubbles: true > both shadow host and other elements
+     * \ that have the sample listener attached (within the containing component)
+     * \ will listen to the event
+     */
+
+    /*
+    this.dispatchEvent(
+      new CustomEvent('clicker', { bubbles: true, composed: false, detail: { message: this.message } })
+    );
+    */
+
+    /**
+     * using template.querySelector.dispatchEvent for internal events
+     *
+     * the below scenario assumes the elements have corresponding event listener attached
+     * with bubbles: false > only elements matches indicator-child will listen to the event
+     *
+     * with bubbles: true > elements and other elements that have the same listener attached
+     * \ (within the child component) will listen to the event
+     */
+
+    this.template
+      .querySelector('.indicator-child')
+      .dispatchEvent(
+        new CustomEvent('clicker', { bubbles: false, composed: false, detail: { message: this.message } })
+      );
+  }
+
+  sectionOnClickHandler() {
+    console.log('>> hd_ChildComponent1; sectionOnClickHandler()');
+    this.message = this.message + 'child section > ';
+  }
+
+  divOnClickHandler() {
+    console.log('>> hd_ChildComponent; divOnClickHandler()');
+    this.message = this.message + 'child div > ';
   }
 }
--- force-app/main/default/lwc/hd_ParentComponent/hd_ParentComponent.js
+++ force-app/main/default/lwc/hd_ParentComponent/hd_ParentComponent.js
@@ -3,13 +3,18 @@ import { LightningElement } from 'lwc';
 export default class hd_ParentComponent extends LightningElement {
   message;
 
-  childOnClickHandler(evt) {
-    console.log('>> childOnClickHandler: The event is now bubbled to hd_ParentComponent');
-    this.message = evt.detail.message + ' from childOnClickHandler';
+  shadowHostOnClickHandler(evt) {
+    console.log('>> shadowHostOnClickHandler(): The event is now bubbled to hd_ParentComponent');
+    this.message = evt.detail.message + 'parent shadow host > ';
   }
 
   divOnClickHandler(evt) {
-    console.log('>> divOnClickHandler: The event is now bubbled to hd_ParentComponent and then to the div as well');
-    this.message = evt.detail.message + ' from divOnClickHandler';
+    console.log('>> divOnClickHandler(): The event is now bubbled to hd_ParentComponent');
+    this.message = evt.detail.message + 'parent div > ';
+  }
+
+  sectionOnClickHandler(evt) {
+    console.log('>> sectionOnClickHandler(): The event is now bubbled to hd_ParentComponent');
+    this.message = evt.detail.message + 'section div > ';
   }
 }
